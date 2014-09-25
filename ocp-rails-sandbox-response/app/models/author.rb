9f4f6982fdef779ae2669989341af71785d51843
Drew Martin <drew.e.martin@gmail.com>
2014-09-24 22:35:22 -0400

    refactored trending method

--- ocp-rails-sandbox-response/app/models/author.rb
+++ ocp-rails-sandbox-response/app/models/author.rb
@@ -10,12 +10,10 @@ class Author < ActiveRecord::Base
     all.each{|x|
       hash[x.id] = x.comments.where("comments.created_at >= ?", Time.zone.now - 7.days).count
     }
-    new_hash = hash.sort_by {|k,v| v}.reverse!
-    new_hash.to_h.delete_if {|k, v| v < 1 } 
-    new_hash.to_h.map do |k,v,|
-      if v > 0
-        self.find(k)
-      end
+    new_hash = hash.sort_by {|k,v| v}.reverse!.to_h
+    new_hash.delete_if {|k, v| v < 1 } 
+    new_hash.map do |k,v,|
+        self.find(k)      
     end
   end
 
--- ocp-rails-sandbox-response/spec/models/author_spec.rb
+++ ocp-rails-sandbox-response/spec/models/author_spec.rb
@@ -1,6 +1,35 @@
 require 'rails_helper'
+require 'spec_helper'
 
 describe Author do
+
+  describe "class methods" do
+
+    it "should be able to call class methods" do
+      @author = FactoryGirl.create(:author)
+      @post = @author.posts.build(id: 1, body:'hello', subject:'hello agains', created_at: Time.now - 10.minutes)
+      @comment1 = @post.comments.build(id: 1, body: 'this is the body', created_at: Time.now - 9.minutes)
+      @comment2 = @post.comments.build(id: 2, body: 'this was the body', created_at: Time.now - 8.minutes)
+      @comment3 = @post.comments.build(id: 3, body: 'this shall be the body', created_at: Time.now - 7.minutes)
+      Author.last.should == @author
+    end 
+    #<Comment id: nil, body: "this is the body", post_id: 1, created_at: nil, updated_at: nil>
+  end
+
+  describe ".trending" do
+
+    it "an instance of Author should be able to return trending" do
+      @author = FactoryGirl.build(:author, name:'drew', created_at: Time.now - 11.years, id: 1)
+      @post = @author.posts.build(id: 1, body:'hello', subject:'hello agains', created_at: Time.now - 10.years)
+      @comment1 = @post.comments.build(id: 1, body: 'this is the body', created_at: Time.now - 9.years)
+      @comment2 = @post.comments.build(id: 2, body: 'this was the body', created_at: Time.now - 8.years)
+      @comment3 = @post.comments.build(id: 3, body: 'this shall be the body', created_at: Time.now - 7.minutes)
+      Author.trending.should == @comment3
+    end 
+    #<Comment id: nil, body: "this is the body", post_id: 1, created_at: nil, updated_at: nil>
+  end
+
+
   context 'before creations' do
     it 'should have a valid factory' do
       author = FactoryGirl.build(:author)
@@ -17,7 +46,7 @@ describe Author do
 
   end
 
-  context 'scopes, class methods, associations' do
+  context 'associations' do
     it "an instance of Author should be able to find its posts" do
       @author = FactoryGirl.build(:author)
       @post = @author.posts.build(body:'hello', subject:'hello agains')
@@ -33,15 +62,11 @@ describe Author do
       @author.comments.should include(@comment1)
     end
 
-    it "an instance of Author should be able to find its posts" do
-      @author = FactoryGirl.build(:author)
-      @post = @author.posts.build(body:'hello', subject:'hello agains')
-      @comment1 = @post.comments.build(body: 'this is the body')
-      @comment2 = @post.comments.build(body: 'this was the body')
-      @comment3 = @post.comments.build(body: 'this shall be the body')
-      @author.comments.should include(@comment1)
-    end
   end
-
-  
+ 
 end
+
+
+
+
+
